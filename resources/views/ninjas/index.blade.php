1b2d96a2d1c3ea8523b56f5238c15bfbc721ce9b
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 10:09:43 +0800

    Created first route.

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{__(":app", ["app" => config("app.name")])}} | Home</title>
+</head>
+<body>
+  <h2>Currently Available Ninjas</h2>
+  <p>{{$greeting}}</p>
+  <ul>
+    <li>Ninjas Here</li>
+  </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // Returns welcome.blade.php from ../resources/views/
+    // Just welcome as Laravel automatically looks for .blade.php files
     return view('welcome');
+});
+
+Route::get("/ninjas", function () {
+    // Returns ninjas.index.blade.php from ../resources/views/ninjas/
+    // $greeting is passed to the view with the key "greeting"
+    return view("ninjas.index", ["greeting" => "Hello"]);
 });
