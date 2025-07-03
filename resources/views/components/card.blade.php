606d3d33337d984b4efdf4ed44c86d607579cf33
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 13:25:06 +0800

    Comments.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,8 @@
-<div class="card">
+{{-- Allows for highlight prop to be accepted with the default value of false. --}}
+@props(["highlight" => false])
+
+{{-- @class is a Blade directive that conditionally applies classes based on @props.--}}
+<div @class(["highlight" => $highlight])>
   {{$slot}}
   {{-- $attributes is a Blade component that allows you to get attributes from a component. --}}
   <a href="{{ $attributes->get("href") }}" class="btn">View Details</a>
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,23 +1,20 @@
-{{--
-  x-filename
-  x-layout is a custom Blade component that outputs the content as {{$slot}} in the layout in components/layout.blade.php.
---}}
+{{-- x-filename --}}
+{{-- x-layout is a custom Blade component that outputs the content as {{$slot}} in the layout in components/layout.blade.php. --}}
 <x-layout>
 
 <h2>Currently Available Ninjas</h2>
 
 <ul>
-  {{--
-    $ninjas comes from /routes/web.php.
-    @foreach/@endforeach are Blade directives that loop through an array
-  --}}
+  {{--$ninjas comes from /routes/web.php.--}}
+  {{-- @foreach/@endforeach are Blade directives that loop through an array --}}
   @foreach($ninjas as $ninja)
     <li>
-    {{-- 
-      Passing a href attribute to the card layout component
-      Must use '' inside of "" link.
-    --}}
-      <x-card href="/ninjas/{{ $ninja['id']}}">
+      {{--Passing a href prop to the card layout component--}}
+      {{-- Must use '' inside of "" link. --}}
+      {{-- 
+        The :highlight is a dynamic prop. Props are normally passed as strings, even with {{}}, but the : prefix allows you to pass other data types. Still written as string though. 
+      --}} 
+      <x-card href="/ninjas/{{ $ninja['id']}}" :highlight="true">
         {{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP.--}}
         <h3>{{ $ninja["name"] }}</h3>
       </x-card>
