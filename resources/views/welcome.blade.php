2c2d8fa683710178718d996132ca83db01fa87b0
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 10:10:00 +0800

    Added link to /ninjas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
+{{-- .blade.php allows for dynamic values with {{}} --}}
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{__(":app", ["app" => config("app.name")])}}</title>
 </head>
 <body>
     <h1>{{ __('Welcome to :app', ['app' => config('app.name')]) }}</h1>
+    <a href="/ninjas">Find Ninjas!</a>
 </body>
 </html>
