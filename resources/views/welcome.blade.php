595c260e2e647e568faad03c769eaca28862dcc5
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 14:56:45 +0800

    Styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/css/app.css')
 </head>
 <body class="text-center px-8 py-12">
-    <h1>{{ __('Welcome to :app', ['app' => config('app.name')]) }}</h1>
+    <h1 class="text-center text-lg text-red-600">{{ __('Welcome to :app', ['app' => config('app.name')]) }}</h1>
     <a href="/ninjas" class="btn mt-4 inline-block">Find Ninjas!</a>
 </body>
 </html>
