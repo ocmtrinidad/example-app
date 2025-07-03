8882cd2bf60eb5351b398f05b0be8bcaab870482
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 12:51:55 +0800

    Updated comments.

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -6,17 +6,15 @@
 
 <h2>Currently Available Ninjas</h2>
 
-{{--The @if / @endif blade directive is used to conditionally display content. --}}
-{{--No { } needed--}}
-@if ($greeting)
-  <p>Hi from inside the if statement.</p>
-@endif
 
-{{--$ninjas comes from /routes/web.php--}}
-{{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP--}}
 <ul>
+  {{--
+    $ninjas comes from /routes/web.php
+    @foreach/@endforeach are Blade directives that loop through an array
+  --}}
   @foreach($ninjas as $ninja)
     <li>
+      {{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP--}}
       <p>{{$ninja["name"]}}</p>
       <a href="/ninjas/{{$ninja["id"]}}">View Details</a>
     </li>
