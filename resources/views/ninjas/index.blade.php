7a8433aa6411c99bfb2be9ac5f6c7a3ddebffacb
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 10:30:34 +0800

    Worked on routes

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -7,9 +7,15 @@
 </head>
 <body>
   <h2>Currently Available Ninjas</h2>
-  <p>{{$greeting}}</p>
   <ul>
-    <li>Ninjas Here</li>
+    <li>
+      {{--$ninjas comes from web.php--}}
+      {{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP--}}
+      <a href="/ninjas/{{$ninjas[0]["id"]}}">{{$ninjas[0]["name"]}}</a>
+    </li>
+    <li>
+      <a href="/ninjas/{{$ninjas[1]["id"]}}">{{$ninjas[1]["name"]}}</a>
+    </li> 
   </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,19 @@ Route::get('/', function () {
 });
 
 Route::get("/ninjas", function () {
+    $ninjas = [
+        ["name" => "mario", "skill" => 75, "id" => 1],
+        ["name" => "luigi", "skill" => 45, "id" => 2],
+    ];
+
     // Returns ninjas.index.blade.php from ../resources/views/ninjas/
     // $greeting is passed to the view with the key "greeting"
-    return view("ninjas.index", ["greeting" => "Hello"]);
+    return view("ninjas.index", ["ninjas" => $ninjas]);
+});
+
+// Route to show a specific ninja by ID
+// The {id} is a route parameter that will be passed to the function as $id
+Route::get("/ninjas/{id}", function ($id) {
+
+    return view("ninjas.show", ["id" => $id]);
 });
