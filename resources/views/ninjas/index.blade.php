6088942fdb711f52b69b15927a3795f9e3bf2205
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-03 13:15:37 +0800

    Using <x-card> layout for displaying ninjas.

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,22 +1,26 @@
 {{--
   x-filename
-  x-layout is a custom Blade component that outputs the content as {{$slot}} in the layout in components/layout.blade.php
+  x-layout is a custom Blade component that outputs the content as {{$slot}} in the layout in components/layout.blade.php.
 --}}
 <x-layout>
 
 <h2>Currently Available Ninjas</h2>
 
-
 <ul>
   {{--
-    $ninjas comes from /routes/web.php
+    $ninjas comes from /routes/web.php.
     @foreach/@endforeach are Blade directives that loop through an array
   --}}
   @foreach($ninjas as $ninja)
     <li>
-      {{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP--}}
-      <p>{{$ninja["name"]}}</p>
-      <a href="/ninjas/{{$ninja["id"]}}">View Details</a>
+    {{-- 
+      Passing a href attribute to the card layout component
+      Must use '' inside of "" link.
+    --}}
+      <x-card href="/ninjas/{{ $ninja['id']}}">
+        {{--Using blade's {{}} syntax is like using echo and htmlspecialchars in PHP.--}}
+        <h3>{{ $ninja["name"] }}</h3>
+      </x-card>
     </li>
   @endforeach
 </ul>
