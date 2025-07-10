e019c13ecfab6e6cc17582611bdb12ba909e7dbf
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-10 10:26:59 +0800

    Added middleware()->controller()->group() to routes

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,31 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NinjaController;
 use App\Http\Controllers\DojoController;
 
-Route::get("/register", [AuthController::class, "showRegister"])->name("show.register");
-Route::get("/login", [AuthController::class, "showLogin"])->name("show.login");
-Route::post("/register", [AuthController::class, "register"])->name("register");
-Route::post("/login", [AuthController::class, "login"])->name("login");
 Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 
-// Calls the "index" method of NinjaController in the App\Http\Controllers folder.
-// ->name("ninjas.index") creates a named route that allows you to call this route by name instead of destination.
-Route::get('/', [NinjaController::class, "index"])->name("ninjas.index");
-Route::get("/ninjas/create", [NinjaController::class, "create"])->name("ninjas.create");
-// Using route model binding, Laravel will automatically fetch the Ninja model instance based on the {ninja} parameter, which is an id.
-// The "show" method will receive the Ninja instance as $ninja.
-Route::get("/ninjas/{ninja}", [NinjaController::class, "show"])->name("ninjas.show");
-Route::post("/", [NinjaController::class, "store"])->name("ninjas.store");
-Route::delete("/ninjas/{ninja}", [NinjaController::class, "destroy"])->name("ninjas.destroy");
+// middleware("guest") sends logged in users to /.
+Route::middleware("guest")->controller(AuthController::class)->group(function () {
+  Route::get("/register", "showRegister")->name("show.register");
+  Route::get("/login", "showLogin")->name("show.login");
+  Route::post("/register", "register")->name("register");
+  Route::post("/login", "login")->name("login");
+});
 
-Route::get("/dojos", [DojoController::class, "index"])->name("dojos.index");
-Route::get("/dojos/create", [DojoController::class, "create"])->name("dojos.create");
-Route::get("/dojos/{dojo}", [DojoController::class, "show"])->name("dojos.show");
-Route::post("/dojos", [DojoController::class, "store"])->name("dojos.store");
-Route::delete("/dojos/{dojo}", [DojoController::class, "destroy"])->name("dojos.destroy");
+// middleware("auth") sends logged out users to /login.
+// controller() gives all Routes the same controller.
+Route::middleware("auth")->controller(NinjaController::class)->group(function () {
+  Route::get('/', "index")->name("ninjas.index");
+  Route::get("/ninjas/create", "create")->name("ninjas.create");
+  // Using route model binding, Laravel will automatically fetch the Ninja model instance based on the {ninja} parameter, which is an id.
+  Route::get("/ninjas/{ninja}", "show")->name("ninjas.show");
+  Route::post("/", "store")->name("ninjas.store");
+  Route::delete("/ninjas/{ninja}", "destroy")->name("ninjas.destroy");
+});
+
+Route::middleware("auth")->controller(DojoController::class)->group(function () {
+  Route::get("/dojos", "index")->name("dojos.index");
+  Route::get("/dojos/create", "create")->name("dojos.create");
+  Route::get("/dojos/{dojo}", "show")->name("dojos.show");
+  Route::post("/dojos", "store")->name("dojos.store");
+  Route::delete("/dojos/{dojo}", "destroy")->name("dojos.destroy");
+});
