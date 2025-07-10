fa12b0f7fc5619858f166406044fad917cd8fe48
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-10 09:36:01 +0800

    Created logout(). Logouts the user and removes user data and all session data from site.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,11 +25,24 @@ class AuthController extends Controller
 
         $user = User::create($validated);
 
-        // Login the user with sessions and cookies.
+        // Login the new user with sessions and cookies.
         Auth::login($user);
 
         return redirect()->route("ninjas.index");
     }
 
     public function login() {}
+
+    public function logout(Request $request)
+    {
+        // Remove all user data from the session and cookies.
+        Auth::logout();
+
+        // Remove all session data.
+        $request->session()->invalidate();
+        // Regenerate the CSRF session token to prevent CSRF attacks.
+        $request->session()->regenerateToken();
+
+        return redirect()->route("show.login");
+    }
 }
